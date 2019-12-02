64c78dca7208150be2da3408633cdde0aa656926
TallysHenrike <tallys.henrike@gmail.com>
2019-12-02 16:53:32 -0300

    Adicionado hashbang nas rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AcessoComponent } from './acesso/acesso.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpResponseInterceptor } from './interceptors/http-response-interceptor';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 @NgModule({
 	declarations: [
@@ -20,7 +21,10 @@ import { HttpResponseInterceptor } from './interceptors/http-response-intercepto
 		BrowserAnimationsModule,
 		ToastrModule.forRoot()
 	],
-	providers: [{ provide: HTTP_INTERCEPTORS, useClass: HttpResponseInterceptor, multi: true }],
+	providers: [
+		{ provide: HTTP_INTERCEPTORS, useClass: HttpResponseInterceptor, multi: true },
+		//{ provide: LocationStrategy, useClass: HashLocationStrategy }
+	],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
